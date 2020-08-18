c0fc910ed4ad2f86b585b361412be47602844d72
Biki Deka <bikidekaantony@gmail.com>
2020-08-18 19:48:52 +0530

    now the canvas can have grid and each cell of the grid is clickable to add color to it.

--- client/src/client.js
+++ client/src/client.js
@@ -18,14 +18,50 @@ const onChatSubmitted = (sock) => (e) => {
 };
 
 //draw on canvas
-const getBoard = (canvas) => {
+const getBoard = (canvas, numCells = 20) => {
   const ctx = canvas.getContext("2d");
+  const cellSize = Math.floor(canvas.width / numCells);
 
-  const fillRect = (x, y, color) => {
+  //here x,y refer to the coordinate of the cell
+  const fillCell = (x, y, color) => {
     ctx.fillStyle = color;
-    ctx.fillRect(x - 10, y - 10, 20, 20);
+    ctx.fillRect(x * cellSize, y * cellSize, cellSize, cellSize);
   };
-  return { fillRect };
+
+  const drawGrid = () => {
+    ctx.strokeStyle = "#333";
+    ctx.beginPath();
+
+    for (let i = 0; i < numCells; i++) {
+      //draw verticles lines
+      ctx.moveTo(i * cellSize, 0);
+      ctx.lineTo(i * cellSize, cellSize * numCells);
+
+      //draw horizontal lines
+      ctx.moveTo(0, i * cellSize);
+      ctx.lineTo(cellSize * numCells, i * cellSize);
+    }
+    ctx.stroke();
+  };
+
+  const clear = () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  };
+
+  const reset = () => {
+    clear();
+    drawGrid();
+  };
+
+  //x,y is coordinate of the mouse click relative to canvas
+  const getCellCoordinates = (x, y) => {
+    return {
+      x: Math.floor(x / cellSize),
+      y: Math.floor(y / cellSize),
+    };
+  };
+
+  return { fillCell, reset, getCellCoordinates };
 };
 
 //gets the coordinate of the mouse click relative to the element,
@@ -43,21 +79,24 @@ const getClickCoordinates = (element, ev) => {
 
 (() => {
   const canvas = document.querySelector("canvas");
-  const { fillRect } = getBoard(canvas);
+  const { fillCell, reset, getCellCoordinates } = getBoard(canvas);
+
   const sock = io.connect("http://localhost:8000");
 
   const onClick = (e) => {
     //gets the coordinate of the mouse click relative to the canvas
     const { x, y } = getClickCoordinates(canvas, e);
-    //emit the coordinates to server so that it is broadcasted to others also
-    sock.emit("turn", { x, y });
+    //emit the cell coordinates to server so that it is broadcasted
+    //to others also
+    sock.emit("turn", getCellCoordinates(x, y));
   };
 
-  /** receiving the data from server and acting accordingly **/
+  reset();
 
+  /** receiving the data from server and acting accordingly **/
   sock.on("message", (text) => log(text));
-  //draw a small pixel like rect when server sends a coordinate
-  sock.on("turn", ({ x, y }) => fillRect(x, y, "green"));
+  //draw a small pixel like rect when server sends a cell coordinate
+  sock.on("turn", ({ x, y, color }) => fillCell(x, y, color));
 
   //declare events
   document
--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const express = require("express");
 const socket = require("socket.io");
+const randomcolor = require("randomcolor");
 
 const app = express();
 
@@ -14,13 +15,15 @@ const server = http.createServer(app);
 const io = socket(server);
 
 io.on("connection", (sock) => {
+  const color = randomcolor();
   sock.emit("message", "You are connected");
 
   //broadcasting the msg sent by one client
   sock.on("message", (text) => io.emit("message", text));
   //whenever a client will do a mouse click on their canvas
-  //we will get their coordinates on canvas ,it is broadcated to others.
-  sock.on("turn", ({ x, y }) => io.emit("turn", { x, y }));
+  //we will get the cell coordinates on canvas ,it is broadcated to others.
+  //also sending each player or socket a unique color
+  sock.on("turn", ({ x, y }) => io.emit("turn", { x, y, color }));
 });
 
 server.on("error", () => {
